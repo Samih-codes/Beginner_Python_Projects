7ee7c523ccb674c5152afcb4c18dec048a77862d
Samih Batt <158607296+Samih-codes@users.noreply.github.com>
2024-02-23 21:38:53 +0000

    Update main.py

--- Team_Name_Generator/main.py
+++ Team_Name_Generator/main.py
@@ -1,1 +1,22 @@
+# My Project Notes
+
+#1. Create a greeting.
+
+#2. Ask the user for the city that they grew up in.
+
+#3. Ask the user for the plural name of an animal.
+
+#4. Combine the name of their city and animal and show them their basketball team name.
+
+#5. Make the input cursor show up on a new line:
+
+Welcome = "Welcome to the basketball team name generator! "
+print(Welcome)
+
+city = input("What city did you grow up in?\n")
+
+animal = input("What is the plural name of your favourite animal?\n")
+
+team_name = city + " " + animal
+print("Your team name could be " + team_name)
 
